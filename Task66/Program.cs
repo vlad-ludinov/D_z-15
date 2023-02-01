7324bea23f70a8fb4fac7c162628ccd5ada64b31
vlad <vladislavludinov3006@gmail.com>
2023-02-01 19:01:07 +0300

    Task66 complete

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите числа промежуток между которыми надо суммировать через запятую: ");
+string [] interval = ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries);
+int sum = findSum(int.Parse(interval[0]), int.Parse(interval[1]));
+WriteLine($"Сумма чисел между {int.Parse(interval[0])} и {int.Parse(interval[1])} = {sum}");
+
+int findSum(int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    else
+    {
+        return M + findSum(M+1,N);
+    }
+}
