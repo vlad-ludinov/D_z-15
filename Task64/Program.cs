055a6cb8e6a65b6f8e20bf6c7d5589719071121a
vlad <vladislavludinov3006@gmail.com>
2023-02-01 18:43:42 +0300

    Task64 complete

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+printNumbers(number);
+
+void printNumbers(int num)
+{
+    if (num == 1) 
+    {
+        Write(num);
+    }
+    else
+    {
+        Write($"{num}, ");
+        printNumbers(num-1);
+    }
+}
