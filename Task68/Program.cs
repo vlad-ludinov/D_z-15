a44eb8a750a4e140f0921caa59b9b85b2223a8b2
vlad <vladislavludinov3006@gmail.com>
2023-02-01 19:39:32 +0300

    Task68 complete

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int accerman = findAccerman(3,2);
+WriteLine(accerman);
+
+int findAccerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return findAccerman(n - 1, 1);
+    else
+      return findAccerman(n - 1, findAccerman(n, m - 1));
+}
